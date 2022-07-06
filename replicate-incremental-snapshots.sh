48fe058128d536fad4e5750a2475635473aac815
Michael Lynch <git@mtlynch.io>
2022-07-05 20:25:07 -0400

    Fix more shellcheck errors

--- replicate-incremental-snapshots.sh
+++ replicate-incremental-snapshots.sh
@@ -8,6 +8,7 @@ mkdir -p "${INCREMENTAL_SNAPSHOTS_DIR}"
 
 # shellcheck disable=SC2153 # Not a misspelling
 for DATASET in "${DATASETS[@]}"; do
+  #  shellcheck disable=SC2012 # ls is better than find in this context
   BASE_SNAPSHOT_FILENAME="$(basename "$(ls -tr "${FULL_SNAPSHOTS_DIR}/${DATASET}"* | tail -1)")"
   BASE_SNAPSHOT="${POOL}/${BASE_SNAPSHOT_FILENAME}"
   LATEST_SNAPSHOT="$(zfs list -t snapshot -o name -s creation -r "${POOL}"/"${DATASET}" | tail -1)"
--- settings.example.sh
+++ settings.example.sh
@@ -2,7 +2,9 @@
 
 readonly POOL="mypool"
 readonly BASE_DIR="/mnt/${POOL}/encrypted-backups"
+# shellcheck disable=SC2034 # Not unused
 readonly FULL_SNAPSHOTS_DIR="${BASE_DIR}/full-snapshots"
+# shellcheck disable=SC2034 # Not unused
 readonly INCREMENTAL_SNAPSHOTS_DIR="${BASE_DIR}/incremental-snapshots"
 
 DATASETS=()
